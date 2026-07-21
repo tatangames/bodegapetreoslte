606648e0df0c9eb63e5d48bc9b13904ea0ac2d45
Tatan <tatangamess@gmail.com>
2026-07-21 11:09:42 -0600

    petreos

--- resources/views/frontend/login/vistalogin.blade.php
+++ resources/views/frontend/login/vistalogin.blade.php
@@ -4,7 +4,7 @@
 @include('backend.urlglobal')
 
 <head>
-    <title>Bienes Municipales</title>
+    <title>Planta Procesador de Petreos</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/login/bootstrap.min.css') }}">
@@ -117,7 +117,7 @@
 
 
                         <!-- TÍTULO  -->
-                        <h4 class="login-title">BIENES MUNICIPALES</h4>
+                        <h4 class="login-title">PLANTA PROCESADOR DE PETREOS</h4>
                         <form class="login-form">
                             <div>
                                 <label class="font-500">Usuario</label>
